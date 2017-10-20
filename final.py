ae0b8996804a49deff8b22caad9c4209a5ed604b
AdriaGS <adiagil.94@gmail.com>
2017-10-20 17:02:59 +0000

    Codi Python

--- final.py
+++ final.py
@@ -6,54 +6,32 @@ try:
     import spidev
 
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(24, GPIO.OUT)
-    GPIO.setup(23, GPIO.OUT)
-    GPIO.setup(16, GPIO.OUT)#LED 1
-    GPIO.setup(20, GPIO.OUT)#LED 2
-    GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)#TXRX
-    GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)#ONOFF
-    GPIO.output(23,1)
-    GPIO.output(24,1)
-    GPIO.output(16,0)
-    GPIO.output(20,0)
-
-    TX_RX=True
+    #GPIO.setup(23, GPIO.OUT)
+    #GPIO.output(23,1)
     
+    
+
+    print("Transmitter")
+    pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+
+    radio = NRF24(GPIO, spidev.SpiDev())
+    radio.begin(0, 22)
+    radio.setPayloadSize(32)
+    radio.setChannel(0x60)
+
+    radio.setDataRate(NRF24.BR_250KBPS)#2MBPS)
+    radio.setPALevel(NRF24.PA_MAX)#MIN)
+    radio.setAutoAck(False)
+    radio.enableDynamicPayloads()
+
+    radio.openWritingPipe(pipes[1])
+    radio.printDetails()
+    print("///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
+
     while True:
-        input_onoff= GPIO.input(15)
-        input_txrx= GPIO.input(14)
-        if(input_onoff==True):
-            time.sleep(1)
-            print("waiting")
-        else:
-            TX_RX=input_txrx
-            break
-            
-        
-            
-    if(TX_RX):
-        print("Transmitter")
-        pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
-
-        radio = NRF24(GPIO, spidev.SpiDev())
-        radio.begin(0, 17)
-        radio.setPayloadSize(32)
-        radio.setChannel(0x60)  # this is the lower channel
-
-        radio.setDataRate(NRF24.BR_250KBPS)#2MBPS)
-        radio.setPALevel(NRF24.PA_HIGH)#MIN)
-        radio.setAutoAck(False)
-        radio.enableDynamicPayloads()
-    ##    radio.enableAckPayload()
-
-        # radio.openReadingPipe(1, pipes[1])
-        radio.openWritingPipe(pipes[1])
-        radio.printDetails()
-        print("///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
-
-        while True:
-            radio.write("PING")
-            time.sleep(1)
+        print("Transmitting Ping")
+        radio.write("PINGDEPRUEBAMTPPROJECTTEAMC")
+        time.sleep(0.01)
         
 except KeyboardInterrupt:
     GPIO.output(23,0)
