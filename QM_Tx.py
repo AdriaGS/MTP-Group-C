607df44b491844dc9f79d1b684129cd1744918a8
AdriaGS <adiagil.94@gmail.com>
2017-10-24 18:33:57 +0000

    TX

--- QM_Tx.py
+++ QM_Tx.py
@@ -10,7 +10,7 @@ try:
     #GPIO.output(23,1)
     
     print("Transmitter")
-    pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+    pipes = [0xe7, 0xe7, 0xe7, 0xe7, 0xe7]
 
     radio = NRF24(GPIO, spidev.SpiDev())
     radio.begin(0, 22)
@@ -22,14 +22,15 @@ try:
     radio.setAutoAck(False)
     radio.enableDynamicPayloads()
 
-    radio.openWritingPipe(pipes[1])
+    radio.openWritingPipe(pipes)
     radio.printDetails()
     print("///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
 
     while True:
         print("Transmitting Ping")
-        radio.write("PINGDEPRUEBAMTPPROJECTTEAMC")
-        time.sleep(0.01)
+        message = list("PINGDEPRUEBAMTPPROJECTTEAMC")
+        radio.write(message)
+        time.sleep(5)
         
 except KeyboardInterrupt:
     GPIO.output(23,0)
