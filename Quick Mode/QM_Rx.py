e064a9b1647175594667bfd0c0f229a3ea941ec0
raspi-MTP <christian_giuly@hotmail.com>
2017-11-10 10:57:28 +0000

    Testing reading functions

--- Quick Mode/QM_Rx.py
+++ Quick Mode/QM_Rx.py
@@ -42,6 +42,6 @@ try:
         print(str_frame)
             
 except KeyboardInterrupt:
-    GPIO.output(23,0)
-    GPIO.output(24,0)
+    GPIO.output(22,0)
+    #GPIO.output(24,0)
     GPIO.cleanup()
