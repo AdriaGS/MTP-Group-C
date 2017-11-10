70d24e9625ec88c13379f4bdc4af9896a36b1b5d
chbs8 <christian.bs.8@gmail.com>
2017-11-10 10:47:24 +0100

    Reading test for transceiver's blocking (or not) functions

---
+++ reading_test.py
@@ -0,0 +1,48 @@
+try:
+
+    import RPi.GPIO as GPIO
+    from lib_nrf24 import NRF24
+    import time
+    import spidev
+
+    GPIO.setmode(GPIO.BCM)
+    #GPIO.setup(23, GPIO.OUT)
+    #GPIO.output(23,1)
+    
+    print("Transmitter")
+    pipes = [0xe7, 0xe7, 0xe7, 0xe7, 0xe7]
+
+    radio = NRF24(GPIO, spidev.SpiDev())
+    radio.begin(0, 22)
+    radio.setPayloadSize(32)
+    radio.setChannel(0x60)
+
+    radio.setDataRate(NRF24.BR_250KBPS)#2MBPS)
+    radio.setPALevel(NRF24.PA_LOW)
+    radio.setAutoAck(False)
+    radio.enableDynamicPayloads()
+
+    radio.openReadingPipe(1, pipes)
+    radio.printDetails()
+    print("///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
+
+    frame = []
+
+    print("Waiting Ping")
+
+    while True:
+        radio.startListening()
+        while not radio.available(0):
+            print("No input data")
+            sleep(2)
+        # radio.read(frame, radio.getDynamicPayloadSize())
+        # str_frame = ""
+        # for c in range(0, len(frame)):
+        #     str_frame += chr(frame[c])
+        # print("Received Message: ")
+        # print(str_frame)
+            
+except KeyboardInterrupt:
+    GPIO.output(23,0)
+    GPIO.output(24,0)
+    GPIO.cleanup()
