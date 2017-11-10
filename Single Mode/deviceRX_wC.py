3259fa01c4cb8b3a61f12b2c4414d5538264d4d0
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-10 18:00:51 +0100

    Update

--- Single Mode/deviceRX_wC.py
+++ Single Mode/deviceRX_wC.py
@@ -130,7 +130,7 @@ def main():
 			if radio_Rx.available(0):
 				radio_Rx.read(ctrlFrame, radio_Rx.getDynamicPayloadSize())
 				print(ctrlFrame)
-				print(ctrl_flag)
+				print(ord(ctrl_flag))
 				if(chr(ctrlFrame[0]) == ord(ctrl_flag)):
 					radio_Tx.write(list("ACK") + list(ctrl_flag))
 					receivedControlPacket = 1
