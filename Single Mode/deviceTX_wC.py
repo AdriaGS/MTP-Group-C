9c14b1d32d0b9b7561e8dd2caeb23512390b1fa9
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-10 17:53:50 +0100

    Update

--- Single Mode/deviceTX_wC.py
+++ Single Mode/deviceTX_wC.py
@@ -194,17 +194,14 @@ def main():
 				radio_Rx.read(ack, radio_Rx.getDynamicPayloadSize())
 				for c in range(0, len(ack)):
 					str_ack = str_ack + chr(ack[c])
-
-				print(str_ack)
-				if(list(str_ack) != (list("ACK") + list(ctrl_flag))):
+				if(ack != (list("ACK") + list(ctrl_flag))):
 					radio_Tx.write(list(ctrlMessage))
 					timeout = time.time() + time_ack
 					print("Control Message Lost")
 					str_ack = ""
 				else:
 					controlAck_received = 1
-
-			if((time.time() + 0.1) > timeout):
+			if((time.time() + 0.01) > timeout):
 				print("No Control ACK received resending message")
 				radio_Tx.write(list(ctrlMessage))
 				timeout = time.time() + time_ack
