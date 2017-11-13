ee9d220c1b53e0f4a3d0f1da201b02745d69e6ff
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-13 18:20:43 +0100

    Update

--- Single Mode/deviceTX_wC_2.py
+++ Single Mode/deviceTX_wC_2.py
@@ -71,8 +71,8 @@ def main():
 	pipe_Tx = [0xe7, 0xe7, 0xe7, 0xe7, 0xe7]
 	pipe_Rx = [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]
 	payloadSize = 32
-	channel_TX = 0x40
-	channel_RX = 0x45
+	channel_TX = 0x60
+	channel_RX = 0x65
 
 	#Initializa the radio transceivers with the CE ping connected to the GPIO22 and GPIO24
 	radio_Tx = NRF24(GPIO, spidev.SpiDev())
