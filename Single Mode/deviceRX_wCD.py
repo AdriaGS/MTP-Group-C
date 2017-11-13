c09c1d98ba8d6ceaf0320e3fa696f79a7293295f
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-14 00:13:13 +0100

    Update

--- Single Mode/deviceRX_wCD.py
+++ Single Mode/deviceRX_wCD.py
@@ -8,32 +8,32 @@ import os.path
 import pickle
 
 def decompress(compressed):
-    """Decompress a list of output ks to a string."""
-    from cStringIO import StringIO
-    # Build the dictionary.
+	"""Decompress a list of output ks to a string."""
+	from cStringIO import StringIO
+	# Build the dictionary.
 	dict_size = 256
 	dictionary = dict((i, chr(i)) for i in xrange(dict_size))
  
-    # use StringIO, otherwise this becomes O(N^2)
-    # due to string concatenation in a loop
-    result = StringIO()
-    w = chr(compressed.pop(0))
-    result.write(w)
-    for k in compressed:
-        if k in dictionary:
-            entry = dictionary[k]
-        elif k == dict_size:
-            entry = w + w[0]
-        else:
-            raise ValueError('Bad compressed k: %s' % k)
-        result.write(entry)
+	# use StringIO, otherwise this becomes O(N^2)
+	# due to string concatenation in a loop
+	result = StringIO()
+	w = chr(compressed.pop(0))
+	result.write(w)
+	for k in compressed:
+		if k in dictionary:
+			entry = dictionary[k]
+		elif k == dict_size:
+			entry = w + w[0]
+		else:
+			raise ValueError('Bad compressed k: %s' % k)
+		result.write(entry)
  
-        # Add w+entry[0] to the dictionary.
-        dictionary[dict_size] = w + entry[0]
-        dict_size += 1
+		# Add w+entry[0] to the dictionary.
+		dictionary[dict_size] = w + entry[0]
+		dict_size += 1
  
-        w = entry
-    return result.getvalue()
+		w = entry
+	return result.getvalue()
 
 def decompressionOnTheGo(compressedFile, multiplicationList):
 
