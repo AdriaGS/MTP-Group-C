0c9ce11a0535036a83f7c90b63b99e363cd7437e
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-15 00:02:24 +0100

    Update

--- Single Mode/deviceRX_wC2.py
+++ Single Mode/deviceRX_wC2.py
@@ -179,8 +179,8 @@ def main():
 					print(len(frame))
 					if (nextIndexing == 0):
 
-						compressed.extend([frame[i] for i in indexing_0])
-						multData.extend([frame[i] for i in indexing_1])
+						compressed.extend([frame[i] for i in [i for i in indexing_0 if i < len(frame)]])
+						multData.extend([frame[i] for i in [i for i in indexing_1 if i < len(frame)]])
 						if(n > 16):
 							multData_extended.extend([frame[i] for i in indexing_2])
 						nextIndexing = (nextIndexing + 1) % int(n/8)+1
