91d08cb65ff5db0b87841587ebc96782bfb9f251
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-15 00:54:09 +0100

    Update

--- Single Mode/deviceRX_wC2.py
+++ Single Mode/deviceRX_wC2.py
@@ -80,8 +80,8 @@ def main():
 	radio_Rx.setChannel(channel_RX)
 
 	#We set the Transmission Rate
-	radio_Tx.setDataRate(NRF24.BR_250KBPS)
-	radio_Rx.setDataRate(NRF24.BR_250KBPS)
+	radio_Tx.setDataRate(NRF24.BR_1MBPS)
+	radio_Rx.setDataRate(NRF24.BR_1MBPS)
 
 	#Configuration of the power level to be used by the transceiver
 	radio_Tx.setPALevel(NRF24.PA_MIN)
