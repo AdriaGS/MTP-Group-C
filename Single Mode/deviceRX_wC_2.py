c2e5e52b11ef4a3da96c73b479144f706c9a6f4d
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-17 16:29:57 +0100

    Update

--- Single Mode/deviceRX_wC_2.py
+++ Single Mode/deviceRX_wC_2.py
@@ -56,7 +56,7 @@ def decompressionOnTheGo(compressedList, listLength, listMax):
 
 	while i < listLength :
 	  if x < bitsMax:
-		strJoin = (strJoin<<charLength) + (compressedList[j])
+		strJoin = (strJoin<<charLength) + ord(compressedList[j])
 		x = x + charLength
 		j = j + 1;
 	  else:
