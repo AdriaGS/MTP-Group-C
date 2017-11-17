b6a97edea63d216d9fae06ca8e28ae1c2bcd2338
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-17 15:58:31 +0100

    Update

--- Single Mode/deviceTX_wC_2.py
+++ Single Mode/deviceTX_wC_2.py
@@ -220,7 +220,7 @@ def main():
 	for message in packets:
 
 		flag = chr(ord(original_flag) + flag_n)
-		message2Send = list(flag) + message
+		message2Send = list(flag) + list(message)
 		radio_Tx.write(message2Send)
 		#time.sleep(1)
 
