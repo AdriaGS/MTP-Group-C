69d5184cd7d3a18f05de2f2799d3e73b43995e7b
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-21 10:35:20 +0100

    Update

--- Single Mode/Semi-Final/deviceTX_wC_2.py
+++ Single Mode/Semi-Final/deviceTX_wC_2.py
@@ -228,6 +228,7 @@ try:
 			flag = chr(ord(original_flag) + flag_n)
 			message2Send = list(flag) + list(message)
 			radio_Tx.write(message2Send)
+			time.sleep(1)
 
 			timeout = time.time() + time_ack
 			radio_Rx.startListening()
