88b721115fde5221ff8e7269b5fee25503921f1b
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-21 15:45:49 +0100

    Update

--- Single Mode/Semi-Final/deviceTX_wC_2.py
+++ Single Mode/Semi-Final/deviceTX_wC_2.py
@@ -120,7 +120,7 @@ try:
 		###############################################################################################################################
 
 		#Read file to transmit
-		inFile = open("ElQuijote.txt", "rb")
+		inFile = open("MTP_Prev.txt", "rb")
 		data2Tx = inFile.read()
 		inFile.close()
 
