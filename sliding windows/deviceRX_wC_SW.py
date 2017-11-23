f4bf3521680454cdd5d0f7cac4d026630083b352
raspi-MTP <christian_giuly@hotmail.com>
2017-11-23 12:05:25 +0100

    sliding windows 2

---
+++ sliding windows/deviceRX_wC_SW.py
@@ -0,0 +1,236 @@
+try:
+	import RPi.GPIO as GPIO
+	from lib_nrf24 import NRF24
+	import time
+	import spidev
+	import numpy as np
+	from threading import Thread
+
+	def decompress(compressed):
+		"""Decompress a list of output ks to a string."""
+		from cStringIO import StringIO
+	
+		# Build the dictionary.
+		dict_size = 256
+		dictionary = {i: chr(i) for i in range(dict_size)}
+		# in Python 3: dictionary = {i: chr(i) for i in range(dict_size)}
+	 
+		# use StringIO, otherwise this becomes O(N^2)
+		# due to string concatenation in a loop
+		result = StringIO()
+		w = chr(compressed.pop(0))
+		result.write(w)
+		for k in compressed:
+			if k in dictionary:
+				entry = dictionary[k]
+			elif k == dict_size:
+				entry = w + w[0]
+			else:
+				raise ValueError('Bad compressed k: %s' % k)
+			result.write(entry)
+	 
+			# Add w+entry[0] to the dictionary.
+			dictionary[dict_size] = w + entry[0]
+			dict_size += 1
+	 
+			w = entry
+		return result.getvalue()
+
+	def decompressionOnTheGo(compressedList, listMax):
+
+		#Open file to save the transmitted data
+		outputFile = open("ReceivedFileCompressed2.txt", "wb")
+
+		compressedString = ""
+		for c in range(0, len(compressedList)):
+			compressedString += chr(compressedList[c])
+
+		i = 0
+		#compressedList += chr(0)
+		strJoin = 0
+		compde = []
+		x = 0
+		j = 0
+		bitsMax = int(np.ceil(np.log(listMax+1)/np.log(2)))
+		charLength = 8
+
+		while i < (len(compressedString)*8/bitsMax):
+			if x < bitsMax:
+				strJoin = (strJoin<<charLength) + ord(compressedString[j])
+				x = x + charLength
+				j = j + 1;
+			else:
+				compde.append(strJoin>>(x-bitsMax))
+				strJoin = strJoin & (2**(x-bitsMax)-1)
+				i += 1
+				x = x - bitsMax
+
+		str_decompressed = decompress(compde)
+		outputFile.write(str_decompressed)
+		outputFile.close()
+
+
+	def main():		
+
+		start = time.time()
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(23, GPIO.OUT)
+		GPIO.output(23,1)
+		GPIO.setup(22, GPIO.OUT)
+		GPIO.output(22,1)
+		GPIO.setup(24, GPIO.OUT)
+		GPIO.output(24,1)
+
+		print("Receiver")
+		pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+		payloadSize = 32
+		channel_RX = 90
+		channel_TX = 100
+
+		#Initializa the radio transceivers with the CE ping connected to the GPIO22 and GPIO24
+		radio_Tx = NRF24(GPIO, spidev.SpiDev())
+		radio_Rx = NRF24(GPIO, spidev.SpiDev())
+		radio_Tx.begin(0, 22)
+		radio_Rx.begin(1, 23)
+
+		#We set the Payload Size to the limit which is 32 bytes
+		radio_Tx.setPayloadSize(payloadSize)
+		radio_Rx.setPayloadSize(payloadSize)
+
+		#We choose the channels to be used for one and the other transceiver
+		radio_Tx.setChannel(channel_TX)
+		radio_Rx.setChannel(channel_RX)
+
+		#We set the Transmission Rate
+		radio_Tx.setDataRate(NRF24.BR_250KBPS)
+		radio_Rx.setDataRate(NRF24.BR_250KBPS)
+
+		#Configuration of the power level to be used by the transceiver
+		radio_Tx.setPALevel(NRF24.PA_LOW)
+		radio_Rx.setPALevel(NRF24.PA_LOW)
+
+		#We disable the Auto Acknowledgement
+		radio_Tx.setAutoAck(False)
+		radio_Rx.setAutoAck(False)
+		radio_Tx.enableDynamicPayloads()
+		radio_Rx.enableDynamicPayloads()
+
+		#Open the writing and reading pipe
+		radio_Tx.openWritingPipe(pipes[1])
+		radio_Rx.openReadingPipe(0, pipes[0])
+
+		#We print the configuration details of both transceivers
+		radio_Tx.printDetails()
+		print("*------------------------------------------------------------------------------------------------------------*")
+		radio_Rx.printDetails()
+		print("*------------------------------------------------------------------------------------------------------------*")
+
+		###############################################################################################################################
+		###############################################################################################################################
+		###############################################################################################################################
+
+		#Flag variables
+		original_flag_data = 'A'
+		flag = ""
+		flag_n = 0
+
+		#Packet related variables
+		frame = []
+		handshake_frame = []
+		compressed = []
+
+		#ACK related variables
+		time_ack = 0.2
+		receivedPacket = 0
+		receivedHandshakePacket = 0
+
+		#We listen for the control packet
+		radio_Rx.startListening()
+		while not (receivedHandshakePacket):
+			str_Handshakeframe = ""
+
+			if radio_Rx.available(0):
+				radio_Rx.read(handshake_frame, radio_Rx.getDynamicPayloadSize())
+				print("Something received")
+
+				for c in range(0, len(handshake_frame)):
+					str_Handshakeframe = str_Handshakeframe + chr(handshake_frame[c])
+
+				#print("Handshake frame: " + str_Controlframe)
+				if(len(str_Handshakeframe.split(",")) == 4):
+					print("Sending ACK")
+					radio_Tx.write(list("ACK"))
+					numberOfPackets, listLength, listMax, slidingWindowsLength = str_Handshakeframe.split(",")
+					listLength = int(listLength)
+					listMax = int(listMax)
+					slidingWindowsLength=int(slidingWindowsLength)
+					numberOfPackets=int(numberOfPackets)
+					break
+				
+#				else:
+#					if(chr(handshake_frame[0]) == original_flag_data):
+#						print("First data packet received")
+#						handshake_frame = handshake_frame[1:len(handshake_frame)]
+#						compressed.extend(handshake_frame)
+#
+#						radio_Tx.write(list("ACK") + list(original_flag_data))
+#						flag_n = (flag_n + 1) % 10
+#						receivedHandshakePacket = 1
+
+		print("The number of data packets that will be transmitted: " + str(numberOfPackets))
+		print("Length of list: " + str(listLength))
+		print("maximum value of list: " + str(listMax))
+		bitsMax = int(np.ceil(np.log(listMax+1)/np.log(2)))
+
+		radio_Rx.startListening()
+		suma = 0
+
+		pending=list(range(numberOfPackets))
+		recived = {}
+		
+		while len(recived)<numberOfPackets:
+
+			timeout = time.time() + time_ack
+			#flag = chr(ord(original_flag_data) + flag_n)
+			r=0
+			ackPacket=list("ACK")
+			while (1):
+
+				if radio_Rx.available(0):
+					
+					radio_Rx.read(frame, radio_Rx.getDynamicPayloadSize())
+					
+					flag=pending[frame[0]-ord(original_flag_data)]
+					recived[flag] = frame[1:len(frame)]
+					r+=1
+					ackPacket+=list(flag)+list(1)
+					
+				if((time.time()) > timeout) or (r==slidingWindowsLength):
+					radio_Tx.write(list("ACK") + list(ackPacket))
+					break
+				
+			#Lanzar thread (OTRA MANERA DE HACERLO TIPO SERVICIO-CLIENTE)
+#			if (((len(compressed)*8) % (bitsMax*300)) == 0):
+#						print("On the way to win")
+#						thread = Thread(target = decompressionOnTheGo, args = (compressed, listMax))
+#						thread.start()
+#							
+#						radio_Tx.write(list("ACK") + list(flag))
+						
+		thread = Thread(target = decompressionOnTheGo, args = (compressed, listMax))
+		thread.start()
+
+		final = time.time()
+		totalTime = final - start
+		print("Total time: " + str(totalTime))
+
+		print("Number of retransmissions = " + str(suma))
+
+	if __name__ == '__main__':
+		main()
+
+except KeyboardInterrupt:
+	GPIO.output(22,0)
+	GPIO.output(23,0)
+	GPIO.output(24,0)
+	GPIO.cleanup()
