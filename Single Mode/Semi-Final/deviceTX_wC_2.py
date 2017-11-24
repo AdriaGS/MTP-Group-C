1c37ef7619ef8f151c47005070e196c68526cd94
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-24 11:45:36 +0100

    Update

--- Single Mode/Semi-Final/deviceTX_wC_2.py
+++ Single Mode/Semi-Final/deviceTX_wC_2.py
@@ -99,8 +99,8 @@ try:
 		radio_Rx.setDataRate(NRF24.BR_250KBPS)
 
 		#Configuration of the power level to be used by the transceiver
-		radio_Tx.setPALevel(NRF24.PA_HIGH)
-		radio_Rx.setPALevel(NRF24.PA_HIGH)
+		radio_Tx.setPALevel(NRF24.PA_LOW)
+		radio_Rx.setPALevel(NRF24.PA_LOW)
 
 		#We disable the Auto Acknowledgement
 		radio_Tx.setAutoAck(False)
