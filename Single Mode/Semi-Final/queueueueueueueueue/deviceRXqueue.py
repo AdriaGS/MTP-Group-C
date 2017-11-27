ac8250b1bb2352e45af77146a944c201454b7e7d
raspi-MTP <christian_giuly@hotmail.com>
2017-11-27 17:57:52 +0100

    +1

--- Single Mode/Semi-Final/queueueueueueueueue/deviceRXqueue.py
+++ Single Mode/Semi-Final/queueueueueueueueue/deviceRXqueue.py
@@ -1,17 +1,19 @@
 try:
-	import RPi.GPIO as GPIO
-	from lib_nrf24 import NRF24
-	from math import *
-	import time
-	import spidev
-	import sys
-	import os.path
-	import pickle
-	import lzw
-	import numpy as np
-	from threading import Thread
-	myQueue = queue.Queue()
-	myQueue2 = queue.Queue()
+    import RPi.GPIO as GPIO
+    from lib_nrf24 import NRF24
+    import math
+    import time
+    import spidev
+    import sys
+    import os.path
+    import pickle
+    import lzw
+    import numpy as np
+    from threading import Thread
+    import queue
+
+    myQueue = queue.Queue()
+    myQueue2 = queue.Queue()
 
     def decodingOnTheGo(listMax):
 
@@ -67,197 +69,197 @@ try:
             dict_size += 1
             w = entry
             myQueue2.task_done()
-	def init()
-		start = time.time()
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(23, GPIO.OUT)
-		GPIO.output(23,1)
-		GPIO.setup(22, GPIO.OUT)
-		GPIO.output(22,1)
-		GPIO.setup(24, GPIO.OUT)
-		GPIO.output(24,1)
+    def init() :
+        start = time.time()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(23, GPIO.OUT)
+        GPIO.output(23,1)
+        GPIO.setup(22, GPIO.OUT)
+        GPIO.output(22,1)
+        GPIO.setup(24, GPIO.OUT)
+        GPIO.output(24,1)
 
-		print("Receiver")
-		pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
-		payloadSize = 32
-		channel_RX = 40
-		channel_TX = 50
+        print("Receiver")
+        pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+        payloadSize = 32
+        channel_RX = 40
+        channel_TX = 50
 
-		#Initializa the radio transceivers with the CE ping connected to the GPIO22 and GPIO24
-		radio_Tx = NRF24(GPIO, spidev.SpiDev())
-		radio_Rx = NRF24(GPIO, spidev.SpiDev())
-		radio_Tx.begin(0, 22)
-		radio_Rx.begin(1, 23)
+        #Initializa the radio transceivers with the CE ping connected to the GPIO22 and GPIO24
+        radio_Tx = NRF24(GPIO, spidev.SpiDev())
+        radio_Rx = NRF24(GPIO, spidev.SpiDev())
+        radio_Tx.begin(0, 22)
+        radio_Rx.begin(1, 23)
 
-		#We set the Payload Size to the limit which is 32 bytes
-		radio_Tx.setPayloadSize(payloadSize)
-		radio_Rx.setPayloadSize(payloadSize)
+        #We set the Payload Size to the limit which is 32 bytes
+        radio_Tx.setPayloadSize(payloadSize)
+        radio_Rx.setPayloadSize(payloadSize)
 
-		#We choose the channels to be used for one and the other transceiver
-		radio_Tx.setChannel(channel_TX)
-		radio_Rx.setChannel(channel_RX)
+        #We choose the channels to be used for one and the other transceiver
+        radio_Tx.setChannel(channel_TX)
+        radio_Rx.setChannel(channel_RX)
 
-		#We set the Transmission Rate
-		#radio_Tx.setDataRate(NRF24.BR_250KBPS)
-		#radio_Rx.setDataRate(NRF24.BR_250KBPS)
-		radio_Tx.setDataRate(NRF24.BR_2MBPS)
-		radio_Rx.setDataRate(NRF24.BR_2MBPS)
+        #We set the Transmission Rate
+        #radio_Tx.setDataRate(NRF24.BR_250KBPS)
+        #radio_Rx.setDataRate(NRF24.BR_250KBPS)
+        radio_Tx.setDataRate(NRF24.BR_2MBPS)
+        radio_Rx.setDataRate(NRF24.BR_2MBPS)
 
-		#Configuration of the power level to be used by the transceiver
-		radio_Tx.setPALevel(NRF24.PA_MAX)
-		radio_Rx.setPALevel(NRF24.PA_MAX)
+        #Configuration of the power level to be used by the transceiver
+        radio_Tx.setPALevel(NRF24.PA_MAX)
+        radio_Rx.setPALevel(NRF24.PA_MAX)
 
-		#We disable the Auto Acknowledgement
-		radio_Tx.setAutoAck(False)
-		radio_Rx.setAutoAck(False)
-		radio_Tx.enableDynamicPayloads()
-		radio_Rx.enableDynamicPayloads()
+        #We disable the Auto Acknowledgement
+        radio_Tx.setAutoAck(False)
+        radio_Rx.setAutoAck(False)
+        radio_Tx.enableDynamicPayloads()
+        radio_Rx.enableDynamicPayloads()
 
-		#Open the writing and reading pipe
-		radio_Tx.openWritingPipe(pipes[1])
-		radio_Rx.openReadingPipe(0, pipes[0])
+        #Open the writing and reading pipe
+        radio_Tx.openWritingPipe(pipes[1])
+        radio_Rx.openReadingPipe(0, pipes[0])
 
-		#We print the configuration details of both transceivers
-		radio_Tx.printDetails()
-		print("*------------------------------------------------------------------------------------------------------------*")
-		radio_Rx.printDetails()
-		print("*------------------------------------------------------------------------------------------------------------*")
-	def main():	    
-		
-		radio_Tx,radio_Rx = init()
+        #We print the configuration details of both transceivers
+        radio_Tx.printDetails()
+        print("*------------------------------------------------------------------------------------------------------------*")
+        radio_Rx.printDetails()
+        print("*------------------------------------------------------------------------------------------------------------*")
+    def main():        
         
-		#Flag variables
-		original_flag_data = 'A'
-		flag = ""
-		flag_n = 0
-		ctrl_flag_n = 0
+        radio_Tx,radio_Rx = init()
+        
+        #Flag variables
+        original_flag_data = 'A'
+        flag = ""
+        flag_n = 0
+        ctrl_flag_n = 0
 
-		#Packet related variables
-		frame = []
-		handshake_frame = []
+        #Packet related variables
+        frame = []
+        handshake_frame = []
 
-		#ACK related variables
-		time_ack = 0.1
-		receivedPacket = 0
-		receivedHandshakePacket = 0
+        #ACK related variables
+        time_ack = 0.1
+        receivedPacket = 0
+        receivedHandshakePacket = 0
 
-	    #Queue
+        #Queue
         global myQueue
         global myQueue2
-		#Open file
-		outputFile = open("RxFile-MTPGroupC.txt", "wb")
-		#starting threads
-		thread = Thread(target = decodingOnTheGo, args = (listMax))
+        #Open file
+        outputFile = open("RxFile-MTPGroupC.txt", "wb")
+        #starting threads
+        thread = Thread(target = decodingOnTheGo, args = (listMax))
         thread.start()
         thread2 = Thread(target = decompresionOnTheGo, args = (listMax, outputFile))
         thread2.start()
 
-		###############################################################################################################################
-		###############################################################################################################################
-		###############################################################################################################################
-			
-		#We listen for the control packet
-		radio_Rx.startListening()
-		while not (receivedHandshakePacket):
-			str_Handshakeframe = ""
+        ###############################################################################################################################
+        ###############################################################################################################################
+        ###############################################################################################################################
+            
+        #We listen for the control packet
+        radio_Rx.startListening()
+        while not (receivedHandshakePacket):
+            str_Handshakeframe = ""
 
-			if radio_Rx.available(0):
-				radio_Rx.read(handshake_frame, radio_Rx.getDynamicPayloadSize())
-				print("Something received")
+            if radio_Rx.available(0):
+                radio_Rx.read(handshake_frame, radio_Rx.getDynamicPayloadSize())
+                print("Something received")
 
-				for c in range(0, len(handshake_frame)):
-					str_Handshakeframe = str_Handshakeframe + chr(handshake_frame[c])
+                for c in range(0, len(handshake_frame)):
+                    str_Handshakeframe = str_Handshakeframe + chr(handshake_frame[c])
 
-				#print("Handshake frame: " + str_Controlframe)
-				if(len(str_Handshakeframe.split(",")) == 4):
-					print("Sending ACK")
-					radio_Tx.write(list("ACK"))
-					checksum, numberOfPackets, listLength, listMax = str_Handshakeframe.split(",")
-					listLength = int(listLength)
-					listMax = int(listMax)
-				
-				else:
-					if(chr(handshake_frame[0]) == original_flag_data):
-						print("First data packet received")
-						handshake_frame = handshake_frame[1:len(handshake_frame)]
-						for char in handshake_frame :
-							myQueue.put(char)
+                #print("Handshake frame: " + str_Controlframe)
+                if(len(str_Handshakeframe.split(",")) == 4):
+                    print("Sending ACK")
+                    radio_Tx.write(list("ACK"))
+                    checksum, numberOfPackets, listLength, listMax = str_Handshakeframe.split(",")
+                    listLength = int(listLength)
+                    listMax = int(listMax)
+                
+                else:
+                    if(chr(handshake_frame[0]) == original_flag_data):
+                        print("First data packet received")
+                        handshake_frame = handshake_frame[1:len(handshake_frame)]
+                        for char in handshake_frame :
+                            myQueue.put(char)
 
-						radio_Tx.write(list("ACK") + list(original_flag_data))
-						flag_n = (flag_n + 1) % 10
-						receivedHandshakePacket = 1
+                        radio_Tx.write(list("ACK") + list(original_flag_data))
+                        flag_n = (flag_n + 1) % 10
+                        receivedHandshakePacket = 1
 
-		print("The number of data packets that will be transmitted: " + numberOfPackets)
-		print("Length of list: " + str(listLength))
-		print("maximum value of list: " + str(listMax))
-		bitsMax = int(np.ceil(np.log(listMax+1)/np.log(2)))
+        print("The number of data packets that will be transmitted: " + numberOfPackets)
+        print("Length of list: " + str(listLength))
+        print("maximum value of list: " + str(listMax))
+        bitsMax = int(np.ceil(np.log(listMax+1)/np.log(2)))
 
-		###############################################################################################################################
-		###############################################################################################################################
-		###############################################################################################################################
+        ###############################################################################################################################
+        ###############################################################################################################################
+        ###############################################################################################################################
 
-		radio_Rx.startListening()
-		suma = 0
+        radio_Rx.startListening()
+        suma = 0
 
-		for i in range(0, int(numberOfPackets)-1):
+        for i in range(0, int(numberOfPackets)-1):
 
-			timeout = time.time() + time_ack
-			flag = chr(ord(original_flag_data) + flag_n)
+            timeout = time.time() + time_ack
+            flag = chr(ord(original_flag_data) + flag_n)
 
-			while not (receivedPacket):
+            while not (receivedPacket):
 
-				if radio_Rx.available(0):
-					radio_Rx.read(frame, radio_Rx.getDynamicPayloadSize())
-					#print(frame)
+                if radio_Rx.available(0):
+                    radio_Rx.read(frame, radio_Rx.getDynamicPayloadSize())
+                    #print(frame)
 
-					if(chr(frame[0]) == flag):
-						for char in frame[1:len(frame)]
-							myQueue.put(char)
-						radio_Tx.write(list("ACK") + list(flag))
-						receivedPacket = 1
-					else:
-						if ((suma % 10) == 0):
-							print("Number of retransmissions increasing: " + str(suma))
-						suma += 1
-						if flag_n == 0:
-							radio_Tx.write(list("ACK") + list('J'))
-						else:
-							radio_Tx.write(list("ACK") + list(chr(ord(original_flag_data) + flag_n-1)))
-						timeout = time.time() + time_ack
+                    if(chr(frame[0]) == flag):
+                        for char in frame[1:len(frame)]:
+                            myQueue.put(char)
+                        radio_Tx.write(list("ACK") + list(flag))
+                        receivedPacket = 1
+                    else:
+                        if ((suma % 10) == 0):
+                            print("Number of retransmissions increasing: " + str(suma))
+                        suma += 1
+                        if flag_n == 0:
+                            radio_Tx.write(list("ACK") + list('J'))
+                        else:
+                            radio_Tx.write(list("ACK") + list(chr(ord(original_flag_data) + flag_n-1)))
+                        timeout = time.time() + time_ack
 
-			flag_n = (flag_n + 1) % 10
-			receivedPacket = 0
+            flag_n = (flag_n + 1) % 10
+            receivedPacket = 0
 
 
-		myQueue.join()      
-	    thread.stop()
+        myQueue.join()      
+        thread.stop()
         myQueue2.join()        
         thread2.stop()
         outputFile.close()
 
-		###############################################################################################################################
-		###############################################################################################################################
-		###############################################################################################################################
+        ###############################################################################################################################
+        ###############################################################################################################################
+        ###############################################################################################################################
 
-		#Compute Cksum
-		textFile = "RxFile-MTPGroupC.txt"
-		command = "cksum " + textFile + " > checksum.txt"
-		os.system(command)
-		checksumFile = open("checksum.txt", 'rb')
-		checksumRx = checksumFile.read()
-		print(checksumRx)
+        #Compute Cksum
+        textFile = "RxFile-MTPGroupC.txt"
+        command = "cksum " + textFile + " > checksum.txt"
+        os.system(command)
+        checksumFile = open("checksum.txt", 'rb')
+        checksumRx = checksumFile.read()
+        print(checksumRx)
 
-		final = time.time()
-		totalTime = final - start
-		print("Total time: " + str(totalTime))
+        final = time.time()
+        totalTime = final - start
+        print("Total time: " + str(totalTime))
 
-		print("Number of retransmissions = " + str(suma))
+        print("Number of retransmissions = " + str(suma))
 
-	if __name__ == '__main__':
-		main()
+    if __name__ == '__main__':
+        main()
 
 except KeyboardInterrupt:
-	GPIO.output(22,0)
-	GPIO.output(23,0)
-	GPIO.output(24,0)
-	GPIO.cleanup()
+    GPIO.output(22,0)
+    GPIO.output(23,0)
+    GPIO.output(24,0)
+    GPIO.cleanup()
