18018b5d2e3503e201f0a68bbfd33c915bb6e1bf
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-29 10:21:51 +0100

    Update

--- Single Mode/Final Code/sw_managing.py
+++ Single Mode/Final Code/sw_managing.py
@@ -1,14 +1,18 @@
+def led_blink(gpio_value, stop_event):
+
+	while(not stop_event.is_set()):
+		GPIO.output(gpio_value, 1)
+		time.sleep(0.5)
+		GPIO.output(gpio_value, 0)
+		time.sleep(0.5)
+
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(22, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(2, GPIO.OUT) #LED 1 TX_RX Running
-GPIO.setup(3, GPIO.OUT) #LED 2 End-of-File
+GPIO.setup(2, GPIO.OUT, GPIO.LOW) #LED 1 TX_RX Running
+GPIO.setup(3, GPIO.OUT, GPIO.LOW) #LED 2 End-of-File
 GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #ON or OFF
 GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Transmit or Receive
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Network Mode
-
-GPIO.output(22, 1)
-GPIO.output(23, 1)
 
 TX0_RX1 = True
 
@@ -16,14 +20,23 @@ while True:
 	input_onoff = GPIO.input(14)
 	input_tx_rx = GPIO.input(15)
 	input_nm = GPIO.input(18)
+
+	#LED Blinking thread
+	led_1 = Event()
+	led_thread = Thread(target = led_blink, args = (2, led_1))
+
 	if(input_onoff == True):
 		time.sleep(1)
 		print("Waiting to start")
-		print("Tx or Rx: " + str(input_txrx))
-		print("Tx or Rx: " + str(input_nm))
+		print("Tx=1 or Rx=0: " + str(input_tx_rx))
+		print("Network Mode: " + str(input_nm))
+
+		led_thread.start()
+
 	else:
-		TX_RX = input_txrx
-		NM = input_nm
+		time.sleep(1)
 		print("Starting Script")
-		print("Tx or Rx: " + str(input_txrx))
-		print("Tx or Rx: " + str(input_nm))
+		print("Tx=1 or Rx=0: " + str(input_tx_rx))
+		print("Network Mode: " + str(input_nm))
+
+		led_1.set()
