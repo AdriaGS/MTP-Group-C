cf46228b6fe4dcdde1f977b29fd665fcd62096e9
Adria Gil Sorribes <adriagil.94@gmail.com>
2017-11-30 16:13:35 +0100

    Update

--- Quick Mode/QM_Tx.py
+++ Quick Mode/QM_Tx.py
@@ -19,7 +19,7 @@ try:
     radio.setPayloadSize(1)
     radio.setChannel(30)
 
-    radio.setDataRate(NRF24.BR_250KBPS)
+    radio.setDataRate(NRF24.BR_2MBPS)
     radio.setPALevel(NRF24.PA_MAX)
     radio.setAutoAck(False)
     radio.enableDynamicPayloads()
